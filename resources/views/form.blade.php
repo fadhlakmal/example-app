71ea66ae492dc77bce777c6f3f5795812069c792
Ainun Nadhifah Syamsiyah <ainunsyamsiyah@gmail.com>
2024-09-19 07:28:33 +0700

    feat: add bootstrap implementation

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -4,23 +4,31 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <title>Form</title>
 </head>
 <body>
     <h1>Isi Form</h1>
     <hr>
-    <form action="/submit" method="POST">
+    <form action="/submit" method="POST" style="width: 80%; margin: 0 auto;">
         @csrf
-        <label for="username">Nama</label>
-        <input type="text" id="username" name="username">
+        <div class="mb-3">
+            <label for="username" class="form-label">Nama</label>
+            <input type="text" id="username" name="username" class="form-control">
+        </div>
+            
+        <div class="mb-3">
+            <label for="alamat" class="form-label">Alamat</label>
+            <textarea type="text" id="alamat" name="alamat" class="form-control"></textarea>
+        </div>
 
-        <label for="alamat">Alamat</label>
-        <textarea type="text" id="alamat" name="alamat"></textarea>
-
-        <label for="password">Password</label>
-        <input type="password" id="password" name="password">
-
-        <input type="submit" value="Submit">
+        <div class="mb-3">
+            <label for="password" class="form-label">Password</label>
+            <input type="password" id="password" name="password" class="form-control">
+        </div>
+            
+        <button type="submit" class="btn btn-primary">Submit</button>
     </form>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
